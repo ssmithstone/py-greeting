34e3cf4154dadf1fbd6c4ecc9e0ba79e2b9f21ad
stephen smithstone <ssmithstone@me.com>
2024-04-10 07:41:04 +1000

    Add current time output to greeting message
    A new function, print_current_time, was added to the main program, greeting.py. This function grabs the current date and time and prints it out in a human-readable format. This output is now included as part of the overall greeting message.

--- greeting.py
+++ greeting.py
@@ -1,9 +1,19 @@
 import sys
+from datetime import datetime
 
 
 def main():
     name = sys.argv[1] if len(sys.argv) > 1 else "Luke Skywalker"
     print(f"Hello, {name}!")
+    print_current_time()
+
+
+def print_current_time():
+    time = datetime.now()
+    print(f"Current time is {time}")
+    print(f"Year {time.year}\nMonth: {time.month}\nDay: {time.day}")
+    print(f"Hour {time.hour}\nMinutes: {time.minute}\nDay: {time.second}")
+    print(f"Formatted date: {time.strftime('%m/%d/%Y')}")
 
 
 if __name__ == '__main__':
